da199afc9ce3b3755d2742fb20ebbddc5de33efa
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-23 23:24:11 +0200

    Update

--- Section8/server.js
+++ Section8/server.js
@@ -18,6 +18,11 @@ const friends = [
   },
 ];
 
+app.use((req, res, next) => {
+    console.log(`${req.method}, ${req.url}`);
+    next();
+})
+
 app.get('/friends', (req, res) => {
     res.status(200).json(friends)
 })
