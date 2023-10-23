e36f694efe5136c924fabd009cf2831052e43c5f
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-23 23:46:31 +0200

    Update

--- Section8/server.js
+++ Section8/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 3000;
@@ -19,28 +19,52 @@ const friends = [
 ];
 
 app.use((req, res, next) => {
-    console.log(`${req.method}, ${req.url}`);
-    next();
-})
+  console.log(`${req.method}, ${req.url}`);
+  next();
+});
 
-app.get('/friends', (req, res) => {
-    res.status(200).json(friends)
-})
+//to set req.body to a js object when the content type is slash json
+app.use(express.json());
 
-app.get('/friends/:friendId', (req, res) => {
-    const friendId = Number(req.params.friendId);
-    const friend = friends[friendId];
-    if(friend){
-        res.status(200).json(friend);
-    }else{
-        res.status(404).json({
+app.post('/friends', (req, res) => {
+    if(!req.body.name){
+        return res.status(400).json({
             status: 'Faild',
-            message: 'NotFound!',
+            message: 'Missing Friend Name'
         })
     }
-    //res.send('Test Route');
-})
+    console.log('req.body is', req.body);
+  const newFriend = { name: req.body.name, id: friends.length };
+  friends.push(newFriend);
+
+  res.status(200).json({
+    status: "Success",
+    data: newFriend,
+  });
+});
+
+app.get("/friends", (req, res) => {
+  res.status(200).json({
+    status: "Success",
+    results: friends.length,
+    data: friends,
+  });
+});
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      status: "Faild",
+      message: "NotFound!",
+    });
+  }
+  //res.send('Test Route');
+});
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
-})
+  console.log(`Listening on port ${PORT}...`);
+});
