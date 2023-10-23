600ee2716fe55fb75e343c11ad545608fbbb7ce4
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-23 21:54:01 +0200

    Update

--- Section8/server.js
+++ Section8/server.js
@@ -3,8 +3,37 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Test Route');
+const friends = [
+  {
+    id: 0,
+    name: "alaa",
+  },
+  {
+    id: 1,
+    name: "hamdy",
+  },
+  {
+    id: 2,
+    name: "mohammadi",
+  },
+];
+
+app.get('/friends', (req, res) => {
+    res.status(200).json(friends)
+})
+
+app.get('/friends/:friendId', (req, res) => {
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+    if(friend){
+        res.status(200).json(friend);
+    }else{
+        res.status(404).json({
+            status: 'Faild',
+            message: 'NotFound!',
+        })
+    }
+    // res.send('Test Route');
 })
 
 app.listen(PORT, () => {
