b627c867fdaea1c2fe3118d55c42a1ac7c8040a7
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-03 18:08:15 +0200

    Update

--- Section11/Improving Performance/server.js
+++ Section11/Improving Performance/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cluster = require('cluster');
 
 function delay(duration){
     const startTime = Date.now();
@@ -9,16 +10,29 @@ function delay(duration){
 }
 
 app.get('/', (req, res) => {
-    res.send('Performance example');
+    res.send(`Performance example ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
-    //Delay response
+    //JSON.stringify({}) => "{}"
+    //JSON.parse("{}") => {}
+
     delay(5000);
-    res.send('Waiting')
+    res.send(`Waiting ${process.pid}`);
 });
 
-const PORT = 3333;
-app.listen(PORT, () => {
-    console.log(`Successfully running on ${PORT}...`);
-})
+if(cluster.isMaster){
+    // If true the code will execute as master process
+    console.log('Master has been started');
+    cluster.fork();
+    cluster.fork();
+}else{
+    // else the code will execute as worker process
+    console.log('Worker proces started');
+
+    const PORT = 3333;
+    app.listen(PORT, () => {
+        console.log(`Successfully running on ${PORT}...`);
+    })
+}
+
