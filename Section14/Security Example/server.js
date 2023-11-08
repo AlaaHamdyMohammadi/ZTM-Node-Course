12d06235eed0200ce78a4eb2e0b6425ed9c6e542
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-08 21:46:43 +0200

    Certificates

--- Section14/Security Example/server.js
+++ Section14/Security Example/server.js
@@ -1,13 +1,25 @@
+const fs = require('fs');
 const path = require("path");
+const https = require('https');
 const express = require("express");
 
 const app = express();
+
+
+app.get('/secret', (req, res) => {
+    return res.send('Personal Secret');
+})
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+
+
 const PORT = 4000;
-app.listen(PORT, () => {
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
     console.log(`app are working on port ${PORT}...`)
 })
