73e991ece017097ced8398e8bb1b10356ff2e046
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-19 19:37:20 +0200

    Update

--- Section14/Security Example/server.js
+++ Section14/Security Example/server.js
@@ -55,7 +55,7 @@ app.use(passport.session()); //help to set up passport
 
 
 function checkLoggedIn(req, res, next) {
-  const isLoggedIn = true;
+  const isLoggedIn = req.isAuthenticated() && req.user;
   if (!isLoggedIn) {
     return res.status(401).json({
       error: "You must log in !",
