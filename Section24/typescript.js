e758c12d5024c7cfce81a850b54b6aa7f432b56e
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-25 13:17:54 +0200

    Update

--- Section24/typescript.js
+++ Section24/typescript.js
@@ -11,3 +11,12 @@ var Size;
 })(Size || (Size = {}));
 ;
 var sizeName = Size.small;
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+var playerDirection = Direction.Down;
+console.log(playerDirection);
--- Section24/typescript.ts
+++ Section24/typescript.ts
@@ -9,3 +9,10 @@ let basket: [string, number] = ["basketball", 10];
 //Enum
 enum Size { small = 1, meduim = 2};
 let sizeName: number = Size.small;
+
+enum Direction{
+    Up, Down, Left, Right
+}
+
+let playerDirection = Direction.Down;
+console.log(playerDirection);
