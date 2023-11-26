c8a7c8a7dc431fbe7d7782368124b159e924b912
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-26 23:58:03 +0200

    Update

--- Section18/single-player/server.js
+++ Section18/single-player/server.js
@@ -10,8 +10,18 @@ server.listen(PORT, () => {
     console.log(`Listening on port ${PORT} ...`);
 });
 
+let readyPlayerCount = 0;
 
 // on function come from event emitter interface that our socket sever uses to register an event listener
 io.on('connection', (socket) => {
     console.log('user connected', socket.id);
+
+    socket.on('ready', () => {
+        console.log('Player ready', socket.id);
+        readyPlayerCount++;
+
+        if(readyPlayerCount === 2){
+            //broadcast('startGame');
+        }
+    })
 })
