0679dfc79d88ce7082a043cbda96d84190a4cec9
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-26 13:27:09 +0200

    Update

--- Section17/server.js
+++ Section17/server.js
@@ -1,29 +1,78 @@
-const express = require('express');
-const { buildSchema } = require('graphql'); // Allows to create schema
-const { graphqlHTTP } = require("express-graphql"); // Middleware function, which responds to graphql queries 
+const express = require("express");
+const { buildSchema } = require("graphql"); // Allows to create schema
+const { graphqlHTTP } = require("express-graphql"); // Middleware function, which responds to graphql queries
 
+// ! => Required
 const schema = buildSchema(`
     type Query{
-        description: String
-        price: Float
+        products: [Product]
+        orders: [Order]
     }
-`)
+    type Product{
+        id: ID!
+        description: String! 
+        reviews: [Review]
+        price: Float!
+    }
+    type Review{
+        rating: Int!
+        comment: String
+    }
+    type Order{
+        date: String!
+        subtotal: Float
+        items: [OrderItem]
+    }
+    type OrderItem{
+        product: Product!
+        quantity: Int!
+    }
+`);
 
 const root = {
-    description: 'Red Shoe',
-    price: 42.12
-}
+  products: [
+    {
+      id: "redshoe",
+      description: "Red Shoe",
+      price: 42.12,
+    },
+    {
+      id: "bluejean",
+      description: "Blue Jean",
+      price: 55.55,
+    },
+  ],
+  orders: [
+    {
+      date: "2005-05-05",
+      subtotal: 90.22,
+      items: [
+        {
+          product: {
+            id: "redshoe",
+            description: 'Old Red Shoe',
+            price: 45.11
+          },
+          quantity: 2,
+        },
+      ],
+    },
+  ],
+};
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: schema,
     rootValue: root,
     graphiql: true,
-}))
+  })
+);
 
 const PORT = 4000;
 
 app.listen(PORT, () => {
-    console.log(`Running GraphQL server on port ${PORT} ...`)
-})
+  console.log(`Running GraphQL server on port ${PORT} ...`);
+});
